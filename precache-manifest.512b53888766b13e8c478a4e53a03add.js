fac89d6e9b76dcbeb69633d9b8dd18bc11164c6b
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2022-10-30 01:58:57 +0300

    Updates

---
+++ precache-manifest.512b53888766b13e8c478a4e53a03add.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ddfdc6b7e659f84a308",
+    "url": "/health_test/static/css/main.75fd2df4.chunk.css"
+  },
+  {
+    "revision": "3ddfdc6b7e659f84a308",
+    "url": "/health_test/static/js/main.3ddfdc6b.chunk.js"
+  },
+  {
+    "revision": "02e73066ff27f2a42269",
+    "url": "/health_test/static/js/1.02e73066.chunk.js"
+  },
+  {
+    "revision": "47ae5afa249e1d6456a7",
+    "url": "/health_test/static/js/runtime~main.47ae5afa.js"
+  },
+  {
+    "revision": "dfb27999bbb2309bc25eb134e7d7f0c6",
+    "url": "/health_test/static/media/bgi-light.dfb27999.jpg"
+  },
+  {
+    "revision": "84baaedfd7cce6746c29aaf9467c34a1",
+    "url": "/health_test/static/media/bgi-dark.84baaedf.jpg"
+  },
+  {
+    "revision": "ffad1fc836ab99573ee8205a2cc96c2f",
+    "url": "/health_test/index.html"
+  }
+];
